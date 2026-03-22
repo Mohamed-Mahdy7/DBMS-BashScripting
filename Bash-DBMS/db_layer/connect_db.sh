09f7d16e0ce769c01eb5d07aa6329258a5effa9c
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-03-22 23:06:34 +0200

    connect_db finished + CURRENT_DB var created

---
+++ Bash-DBMS/db_layer/connect_db.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/bash
+
+connectDB() {
+    echo 'type "exit" if you want to exit connect DB'
+    read -p "Enter DB Name: " db_name
+
+    if [[ $db_name =~ ^[Ee][Xx][Ii][Tt]$ ]]
+    then
+        echo exiting...
+        CURRENT_DB=""
+        echo CURRENT_BD $CURRENT_DB
+        return 0
+    fi
+
+    if [[ -d "$BASE_DIR/$db_name" ]]
+    then
+        CURRENT_DB=$BASE_DIR/$db_name
+        echo CURRENT_BD $CURRENT_DB
+        echo Connected to $db_name
+        # call table_menu here
+        return 0
+    else
+        echo "No DB with that name!"
+        echo "Make sure you entered the name correctly"
+        return 1
+    fi
+}
