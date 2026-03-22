e28ce784ec8aa5899f6a5c022871bed600d2fb92
Mohamed Mahdy <mohamedmahdy648@gmail.com>
2026-03-22 17:57:12 +0200

    create_db.sh finished, test.sh to test as main.sh

--- Bash-DBMS/db_layer/create_db.sh
+++ Bash-DBMS/db_layer/create_db.sh
@@ -1,1 +1,24 @@
-c
+#! /usr/bin/bash
+
+createFunction() {
+read -p "Enter DataBase Name: " db_name
+
+if [[ $db_name =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]] 
+then
+    echo Valid DB Name
+
+    if [[ -d "$BASE_DIR/$db_name" ]]
+    then
+        echo DB with that name already exist!
+        return 1
+    else
+        echo DB initiated successfully!
+        mkdir $BASE_DIR/$db_name
+    return 0
+    fi
+else
+    echo Name not valid!
+    echo "DB must start with letter or underscore, only letters/digits/underscores allowed"
+    return 1
+fi
+}
